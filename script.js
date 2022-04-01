fa6c748040604fd97f092c8bcd2afc61bf54071d
nicolas <nicolas.lima03@hotmail.com>
2022-03-31 21:53:59 -0300

    implement backspace function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const clearButton = document.querySelector('.clear-button');
 const display = document.getElementById('display-text');
 
+const backspace = document.getElementById('backspace');
+
 //adds a click listener to the 'Clear' button
 clearButton.addEventListener('click', clearDisplay);
 
@@ -8,3 +10,7 @@ function clearDisplay() {
     //erases anything that is in the display.
     display.textContent = '';
 }
+
+backspace.addEventListener('click', () => {
+    display.textContent = display.textContent.slice(0, display.textContent.length -1);
+})
