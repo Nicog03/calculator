8754d280d0885d68dac34c555eca67d33ace8d90
nicolas <nicolas.lima03@hotmail.com>
2022-03-31 21:26:57 -0300

    add a 'clear display' function

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-// let answer;
-// do {
-//     let firstNumber = parseInt(prompt("Choose the first number: "));
-//     if (!isNaN(firstNumber)) {
-//         let operationChoice = prompt("Choose the operation [+][-][*][/]");
-//         if (operationChoice == '+' || operationChoice == '-' || operationChoice == '*' || operationChoice == '/') {
-//             let secondNumber = parseInt(prompt("Choose the second number: "));
-//             if (!isNaN(secondNumber)) {
-//                 switch (operationChoice) {
-//                     case '+':
-//                         console.log(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
-//                         break;
-                    
-//                     case '-':
-//                         console.log(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`)
-//                         break;
-                
-//                     case '*':
-//                         console.log(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`)
-//                         break;
-                
-//                     case '/':
-//                         console.log(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`)
-//                         break;
-//                 }
-//                 answer = prompt('Start another operation? (y/n)').toLowerCase();
-//             } else { console.error('[ERROR] - Please select a valid value.') }
-//         } else { console.error('[ERROR] - Please select a valid value.'); }
-//     } else { console.error('[ERROR] - Please select a valid value.') }
-// } while (answer == 'y');
+const clearButton = document.querySelector('.clear-button');
+const display = document.getElementById('display-text');
 
+//adds a click listener to the 'Clear' button
+clearButton.addEventListener('click', clearDisplay);
 
+function clearDisplay() {
+    //erases anything that is in the display.
+    display.textContent = '';
+}
